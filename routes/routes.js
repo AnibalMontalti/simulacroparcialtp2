056e19d832241eda899f82cb7d5e7a43d16f8a7b
AnibalMontalti <montaltianibal17@gmail.com>
2024-06-18 22:26:28 -0300

    parcial tp2

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import WordController from "../controllers/WordController.js";
+
+const routes = Router()
+const wordController = new WordController()
+
+routes.post('/', wordController.insertWord)
+routes.get("/", wordController.getWords)
+routes.get("/:cantidad", wordController.getWordsApi)
+routes.delete("/",)
+
+export default routes
